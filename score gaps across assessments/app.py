d409d41dc71628c6d2d036973f762bc55a11383d
Alexander MacIntosh <alexandermacintosh@Alexanders-MacBook-Pro.local>
2024-08-20 16:16:15 -0400

    added streamlit app for effect size across assessments

---
+++ score gaps across assessments/app.py
@@ -0,0 +1,165 @@
+import streamlit as st
+import pandas as pd
+
+# Set page layout to wide
+st.set_page_config(layout="wide")
+
+# Load your dataframe
+merged_df = pd.read_csv('data/merged_data.csv')
+
+# Order of subjects for the table
+subjects_ordered = [
+    'NAEP - Science - 4',
+    'NAEP - Reading - 4',
+    'NAEP - Reading - 8',
+    'NAEP - Science - 8',
+    'NAEP - Science - 12',
+    'NAEP - Reading - 12',
+    'SAT - Total',
+    'SAT - Math',
+    'SAT - ERW',
+    'Casper',
+    'MCAT Total',
+    'MCAT CPBS',
+    'MCAT CARS',
+    'MCAT BBLS',
+    'MCAT PSBB',
+    'AAMC - GPA Total',
+    'AAMC - GPA Science',
+    'AAMC - GPA Non-Science',
+    'GRE - Analytical Writing',
+    'GRE - Quantitative',
+    'GRE - Verbal',
+    'GMAT - Total Score',
+    'LSAT'
+]
+
+# Define the race order
+race_order = [
+    'Black',
+    'Hispanic',
+    'Asian',
+    'American Indian/Alaska Native',
+    'Native Hawaiian/Other Pacific Islander',
+    'Another Race/Ethnicity',
+    'Multiple Races/Ethnicities',
+    'White'
+]
+
+# Sidebar for user input
+st.sidebar.header("Filter Data")
+selected_variables = st.sidebar.multiselect("Select Variable", merged_df['Variable'].unique())
+selected_jurisdictions = st.sidebar.multiselect("Select Jurisdiction", merged_df['Jurisdiction'].unique())
+selected_subjects = st.sidebar.multiselect("Select Subjects", merged_df['Subject'].unique())
+ 
+
+# Add a selector for color gradient
+color_map = st.sidebar.selectbox("Select Color Gradient (cmap)", 
+    ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu', 
+     'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'])
+
+# Filtering the dataframe based on user selection
+if not selected_subjects:
+    selected_subjects = merged_df['Subject'].unique()
+    
+filtered_df = merged_df[
+    (merged_df['Variable'].isin(selected_variables)) & 
+    (merged_df['Jurisdiction'].isin(selected_jurisdictions)) &
+    (merged_df['Subject'].isin(selected_subjects))
+]
+
+# Convert year into category using .loc to avoid SettingWithCopyWarning
+filtered_df.loc[:, 'Year'] = filtered_df['Year'].astype('category')
+
+# Create the table with the desired columns
+summary_df = filtered_df[['Variable','Subject', 'Jurisdiction', 'Year', 'Grouping', 'Mean', 'SD', 'N']]
+
+# Rename columns for clarity
+summary_df = summary_df.rename(columns={
+    'Variable': 'Selected Variable',
+    'Jurisdiction': 'Selected Jurisdiction'
+})
+
+# Sort the dataframe to merge 'Selected Variable' across rows
+summary_df = summary_df.sort_values(by=['Selected Variable', 'Selected Jurisdiction', 'Year', 'Grouping'])
+
+# Display the summary dataframe in Streamlit
+with st.expander("Summary Table"):
+    st.dataframe(summary_df)
+
+
+# Create combinations of selected variables and jurisdictions
+combinations = [(var, jur) for var in selected_variables for jur in selected_jurisdictions]
+
+# Create tabs for each combination of selected variables and jurisdictions within an expander
+with st.expander("Effect Size Summary"):
+    if combinations:
+        tabs = st.tabs([f"{var} - {jur}" for var, jur in combinations])
+        for idx, (variable, jurisdiction) in enumerate(combinations):
+            with tabs[idx]:
+                # Filter the dataframe for the current combination of variable and jurisdiction
+                combination_df = filtered_df[(filtered_df['Variable'] == variable) & (filtered_df['Jurisdiction'] == jurisdiction)]
+                
+                # Creating a pivot table for Cohen's d
+                pivot_df = combination_df.pivot_table(
+                    index=['Subject', 'Year'], 
+                    columns='Grouping', 
+                    values="Cohen's d",
+                    aggfunc='first'
+                )
+
+                # Reindex the rows to match the specified order
+                pivot_df = pivot_df.reindex(subjects_ordered, level=0)
+
+                # Conditionally reorder columns if the variable is 'Race/Ethnicity'
+                if variable == 'Race/Ethnicity' and jurisdiction == 'US':
+                    pivot_df = pivot_df[race_order]
+
+                # Combine 'Subject' and 'Year' into a single column for the index
+                pivot_df.index = pivot_df.index.map(lambda x: f'{x[0]} ({x[1]})')
+
+                # Resetting the index for a cleaner look (optional)
+                pivot_df = pivot_df.reset_index()
+
+                # Apply a color gradient and highlight NaN values for Cohen's d table
+                styled_df = pivot_df.style.background_gradient(cmap=color_map, axis=None).format("{:.2f}", subset=pd.IndexSlice[:, pivot_df.select_dtypes(include='number').columns])
+
+                # Add title to the Cohen's d table
+                styled_df = styled_df.set_caption(f"Cohen's d for {jurisdiction} - {variable}")
+
+                # Display the styled dataframe for Cohen's d in Streamlit
+                st.dataframe(styled_df)
+    else:
+        st.write("Please select at least one Variable and Jurisdiction.")
+
+
+
+# Notes section
+with st.expander("Notes"):
+    st.markdown("""
+    **Data Sources**  
+    Annual reports for the following sources were available. Most recent data used where possible.
+
+    **Assessment Sources:**
+    - **SAT**: [SAT Suite Program Results](https://reports.collegeboard.org/sat-suite-program-results)  
+      - Using Pooled SD: [Total Group SAT Suite of Assessments Annual Report](https://reports.collegeboard.org/media/pdf/2023-total-group-sat-suite-of-assessments-annual-report%20ADA.pdf)
+    - **NAEP**: [NAEP API Documentation](https://www.nationsreportcard.gov/api_documentation.aspx)  
+      - See information below for pooled sample size
+    - **LSAT**: [LSAT Research Report](https://www.lsac.org/sites/default/files/research/TR-24-01.pdf)
+    - **GRE**: [GRE Snapshot Report](https://www.ets.org/pdfs/gre/snapshot.pdf#page=11.09)
+    - **GMAT**: [GMAT Test Taker Data](https://www.gmac.com/-/media/files/gmac/research/gmat-test-taker-data/profile-of-gmat-testing-north-america-ty2019-ty2023.pdf)  
+      - Using Pooled SD: [GMAT Validity and Testing Report](https://www.gmac.com/-/media/files/gmac/research/validity-and-testing/rr-17-01-differential-validity-talento-miller-web-release-2.pdf#page=6.09)
+    - **Casper**: [Casper Technical Manual](https://acuityinsights.com/resource/casper-technical-manual/)  
+      - More detailed information derived from Qlik
+    - **AAMC - MCAT GPA**: [AAMC 2023 Facts - Applicants and Matriculants Data](https://www.aamc.org/data-reports/students-residents/data/2023-facts-applicants-and-matriculants-data)
+
+    **NAEP Sample Size:**
+    - **2019 Reading**
+      - Grade 12: N = 26,700 ([Source](https://www.nationsreportcard.gov/highlights/reading/2019/))
+      - Grade 8: N = 143,100 ([Source](https://www.nationsreportcard.gov/highlights/reading/2019/))
+      - Grade 4: N = 150,600 ([Source](https://www.nationsreportcard.gov/highlights/reading/2019/))
+    - **2019 Science**
+      - Grade 4: N = 30,400 ([Source](https://www.nationsreportcard.gov/science/?grade=4))
+      - Grade 8: N = 31,400 ([Source](https://www.nationsreportcard.gov/science/?grade=4))
+      - Grade 12: N = 26,400 ([Source](https://www.nationsreportcard.gov/science/?grade=4))
+    """)
---
+++ score gaps across assessments/utils.py
@@ -0,0 +1,202 @@
+"Module containing helper functions for the ETL pipeline."
+import time
+from datetime import datetime, timedelta
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import pandas as pd
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.errors import HttpError
+
+import config
+
+GOOGLE_SERVICE_ACCOUNT_CREDENTIALS = config.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS
+
+def connect_to_google(type):
+    """Connect to Google Drive or Google Sheets API.
+    
+    Args:
+        type (str): 'drive' or 'sheets'
+        
+    Returns:
+        service: Google Drive or Google Sheets API service instance.
+    """
+    
+    creds = service_account.Credentials.from_service_account_info(GOOGLE_SERVICE_ACCOUNT_CREDENTIALS)
+    if type == 'drive':
+        service = build('drive', 'v3', credentials = creds)
+    elif type == 'sheets':
+        service = build('sheets', 'v4', credentials = creds)
+    else:
+        raise ValueError('Type must be "drive" or "sheets"')
+        
+    return service
+
+def read_google_sheet(service, spreadsheet_id, sheet_name = None, max_retries = 5,
+                      retry_delay = 60):
+    """Read Google Sheet to pandas DataFrame
+    
+    Args:
+        service (googleapiclient.discovery.Resource): Google Sheets service
+        spreadsheet_id (str): Google Sheet ID
+        sheet_name (str): Name of sheet to read; defaults to None
+        max_retries (int): Maximum number of times to retry reading the sheet; defaults to 5
+        retry_delay (int): Number of seconds to wait between retries; defaults to 60
+        
+    Returns:
+        pandas.DataFrame: Dataframe of Google Sheet
+    """
+    
+    if sheet_name is None:
+        file = service.spreadsheets().get(spreadsheetId = spreadsheet_id).execute()
+        sheet_name = file['sheets'][0]['properties']['title']
+    range_name = f"{sheet_name}!A1:ZZ"
+    
+    # sometimes the API fails to read the sheet, so we'll try a few times
+    for attempt in range(max_retries):
+        try:
+            result = service.spreadsheets().values().get(spreadsheetId = spreadsheet_id,
+                                                          range = range_name).execute()
+            values = result.get('values', [])
+            df = pd.DataFrame(values[1:], columns = values[0])
+            break
+        except HttpError as e:
+            if e.resp.status in [408, 504]:  # 408 Request Timeout, 504 Gateway Timeout
+                print(f"Timeout error encountered. Attempt {attempt + 1} of {max_retries}. \
+                    Retrying in {retry_delay} seconds.")
+                time.sleep(retry_delay)
+            else:
+                raise  # Re-raise the exception if it's not a timeout error
+            
+    return df
+
+
+def descriptive_counts_and_percents(df, group_var, target_var):
+    """
+    Create a descriptive DataFrame with counts and percent for each level of a grouping variable by a target variable.
+
+    Parameters:
+    - df: pandas DataFrame containing the data.
+    - group_var: string, the name of the column to group by (e.g., 'Age Group').
+    - target_var: string, the name of the target column (e.g., 'offered').
+
+    Returns:
+    - A pandas DataFrame with counts and percent for each level of group_var by target_var.
+    """
+    # Group by group_var and target_var, then count
+    counts = df.groupby([group_var, target_var]).size()
+
+    # Unstack for better readability
+    counts_unstacked = counts.unstack(level=target_var, fill_value=0)
+
+    # Calculate percent
+    percent = counts_unstacked.div(counts_unstacked.sum(axis=1), axis=0) * 100
+
+    # Combine counts and percent into a single DataFrame for descriptive purposes
+    descriptive_df = pd.concat([counts_unstacked, percent], axis=1, keys=['Count', 'Percent'])
+
+    # Optional: Round the percent values to 2 decimal places
+    descriptive_df = descriptive_df.round(2)
+
+    return descriptive_df
+
+
+def calculate_cohens_d(df, group_var, target_var, subset_var=None, subset_val=None):
+    """
+    Calculate Cohen's d effect size for a target variable across groups defined in a grouping variable,
+    using the most frequently occurring group as the reference, for the whole sample and a subset,
+    and return a DataFrame with both Cohen's d values and their difference.
+
+    Parameters:
+    - df: pandas DataFrame containing the data
+    - group_var: string, the name of the column in df that contains the grouping variable
+    - target_var: string, the name of the column in df for which to calculate Cohen's d
+    - subset_var: string, the name of the column in df for subsetting (optional)
+    - subset_val: value, the value in subset_var to filter by (optional)
+
+    Returns:
+    A pandas DataFrame with group comparisons as rows, Cohen's d values for the whole sample and the subset,
+    and the difference between the two Cohen's d values, rounded to 2 decimal places.
+    """
+    # Find the most frequent group in the grouping variable
+    most_frequent_group = df[group_var].mode()[0]
+    
+    # Initialize a list to store results
+    results = []
+    
+    # Get unique groups excluding the most frequent one
+    unique_groups = df[group_var].unique()
+    unique_groups = [group for group in unique_groups if group != most_frequent_group]
+    
+    # Reference group data
+    reference_data = df[df[group_var] == most_frequent_group][target_var]
+    
+    # Calculate Cohen's d for each group compared to the most frequent group for the whole sample
+    for group in unique_groups:
+        group_data = df[df[group_var] == group][target_var]
+        d_whole = pg.compute_effsize(group_data, reference_data, eftype='cohen')
+        
+        # Calculate Cohen's d for the subset if specified
+        if subset_var and subset_val is not None:
+            subset_df = df[df[subset_var] == subset_val]
+            subset_reference_data = subset_df[subset_df[group_var] == most_frequent_group][target_var]
+            subset_group_data = subset_df[subset_df[group_var] == group][target_var]
+            d_subset = pg.compute_effsize(subset_group_data, subset_reference_data, eftype='cohen')
+        else:
+            d_subset = None
+        
+        # Append results
+        results.append({
+            "Comparison": f"{most_frequent_group} vs {group}",
+            "Cohen's d (Whole Sample)": d_whole,
+            "Cohen's d (Subset)": d_subset,
+            "Difference": d_whole - d_subset if d_subset is not None else None
+        })
+        
+    # Convert results to DataFrame and round values
+    results_df = pd.DataFrame(results).round({'Cohen\'s d (Whole Sample)': 2, 'Cohen\'s d (Subset)': 2, 'Difference': 2})
+
+        
+    return results_df
+
+# Order of subjects for the table
+subjects_ordered = [
+    'NAEP - Science - 4',
+    'NAEP - Reading - 4',
+    'NAEP - Reading - 8',
+    'NAEP - Science - 8',
+    'NAEP - Science - 12',
+    'NAEP - Reading - 12',
+    'SAT - Total',
+    'SAT - Math',
+    'SAT - ERW',
+    'Casper',
+    'MCAT Total',
+    'MCAT CPBS',
+    'MCAT CARS',
+    'MCAT BBLS',
+    'MCAT PSBB',
+    'AAMC - GPA Total',
+    'AAMC - GPA Science',
+    'AAMC - GPA Non-Science',
+    'GRE - Analytical Writing',
+    'GRE - Quantitative',
+    'GRE - Verbal',
+    'GMAT - Total Score',
+    'LSAT'
+]
+
+# Define the race order
+race_order = [
+    'Black',
+    'Hispanic',
+    'Asian',
+    'American Indian/Alaska Native',
+    'Native Hawaiian/Other Pacific Islander',
+    'Multiple Races/Ethnicities',
+    'White'
+]
